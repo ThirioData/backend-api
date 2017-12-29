013e4c173b897cd4efe67c887428be949bfdbd75
OoOO <nirajgeorgian01@gmail.com>
2017-12-29 17:14:04 +0530

    port bounce

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Food, '/food/<string:food_name>')
 api.add_resource(FoodList, '/foods')
 
-port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
