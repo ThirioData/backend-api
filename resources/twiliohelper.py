7d0dad1240f9c6d632af172e8696e3567059294b
OoOO <nirajgeorgian01@gmail.com>
2018-02-10 00:48:35 +0530

    verifyotp

--- resources/twiliohelper.py
+++ resources/twiliohelper.py
@@ -3,17 +3,17 @@ from twilio.rest import Client
 import random
 
 class TwilioHelper:
-    def generate_code():
+    def generate_code(self):
         return str(random.randrange(100000, 999999))
     
-    def send_sms(to_number, body):
+    def send_sms(self, to_number, body):
         account_sid = app.config['TWILIO_ACCOUNT_SID']
         auth_token = app.config['TWILIO_AUTH_TOKEN']
         twilio_number = app.config['TWILIO_NUMBER']
         client = Client(account_sid, auth_token)
         client.api.messages.create(to_number, from_=twilio_number, body=body)
 
-    def send_confirmation_code(to_number):
+    def send_confirmation_code(self, to_number):
         verification_code = generate_code()
         send_sms(to_number, verification_code)
         session['verification_code'] = verification_code
